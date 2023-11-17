a44b5fddb3bd77b7388bfbabcaefe8813593669f
imfarhat <146313122+imfarhat@users.noreply.github.com>
2023-11-17 16:23:33 +0530

    setting laoding sttribute valy to lazy for imgs

--- school/scripts/imported/index.js
+++ school/scripts/imported/index.js
@@ -3,13 +3,17 @@ import { largeNavInteractor, handleNavBarsClick } from "../components/index.js";
 document.addEventListener("DOMContentLoaded", () => {
   try {
     largeNavInteractor();
+    let allImages = document.querySelectorAll('img');
+    allImages.forEach(img => {
+      if (!img.loading || img.loading !== 'lazy') {
+        img.loading = 'lazy';
+      }
+    });
     const smallNavBtn = document.getElementById('js-small-nav-btn');
     smallNavBtn.addEventListener('click', () => {
-    handleNavBarsClick();
+      handleNavBarsClick();
     });
-
-  }
-  catch (error) {
-    console.error("An error occurred:", error);
+  } catch (error) {
+    console.error("An error occurred while applying nav interactions, nav bar clicks & setting the loading attribute to lazy for all imgs:", error);
   }
 });
