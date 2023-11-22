1d0b348fa20790558f8d7f60c2dabc2ce1b0e6fb
imfarhat <imfarhat.dev@gmail.com>
2023-11-22 21:41:43 +0530

    removed school file

--- school/scripts/utility/pages.js
+++
@@ -1,6 +0,0 @@
-const currentTab = document.getElementById('page-id').innerHTML;
-function setTabState(tabId) {
-  const navTab = document.getElementById(`nav-item-${tabId}`);
-  navTab.classList.add('border-t-4', 'text-blue-600', 'bg-white/50');
-};
-setTabState(currentTab);
